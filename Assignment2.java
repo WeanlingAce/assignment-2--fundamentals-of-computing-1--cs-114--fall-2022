3d408291117dc80092fbddffdf887f5ca7e1487c
Desperate-Hit <Weanlingace@gmail.com>
2022-10-05 08:59:10 -0400

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,37 @@
+import java.util.Scanner;
+
 public class Assignment2 {
+
     public static void main(String[] args) {
+        int rows, columns,raws;
+
+        System.out.println("Type a Number");
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+
+        int count=1;
+        for(rows = 1; rows <=2*x-1; rows++) {
+            for(columns = count; columns<=x; columns++)
+            {
+                System.out.println(" ");
+
+            }
+            for(raws = 1;raws<=2*x-1; raws++)
+            {
+                System.out.println("*");
+            }
+
+            System.out.println("\n");
+
+        if (rows<x)
+                count++;
+        else
+                count--;
+                System.out.println("");
+
+
+            }
+
+        }
+
     }
-}
